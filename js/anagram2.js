7117552d670218be593e7c946e0fed15d7e9eb2a
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-05-29 09:14:15 -0700

    solved in js and py

--- js/anagram2.js
+++ js/anagram2.js
@@ -1,3 +1,12 @@
 exports.anagramsFor = function(word, listOfWords) {
+    let answerArr = [];
+    let sortWord = word.split("").sort().join("");
 
+    for (i=0;i<listOfWords.length;i++) {
+       let iSort = listOfWords[i].split("").sort().join("");
+       if (sortWord === iSort) {
+           answerArr.push(listOfWords[i])
+       };
+    };
+    return answerArr;
 };
--- python/anagram2.py
+++ python/anagram2.py
@@ -1,4 +1,14 @@
 # Don't forget to run the tests (and create some of your own)
 
 def anagrams_for(word, list_of_words):
+	answerArr = []
+	wordSort = ''.join(sorted(word))
+
+	for list in list_of_words:
+		sortList = ''.join(sorted(list))
+		if wordSort == sortList:
+			answerArr.append(list)
+	return answerArr
+
+
 		# your code here
